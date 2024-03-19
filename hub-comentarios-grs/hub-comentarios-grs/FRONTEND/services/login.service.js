43cc3c72ca0b816d7345627cad9203ed5c2ce7c1
SantosRgabriel <gabrielsantos.sgra@gmail.com>
2024-03-19 19:22:13 -0300

    Create loginService

---
+++ hub-comentarios-grs/hub-comentarios-grs/FRONTEND/services/login.service.js
@@ -0,0 +1,27 @@
+const URL_API = "http://localhost:7000";
+
+const LoginService = {
+  apiAuthLogin: async (user) => {
+    return new Promise((resolve, reject) => {
+      fetch(`${URL_API}/login`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(user),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          if (data.success) {
+            resolve("Usuario autenticado com sucesso");
+          } else {
+            reject("Erro na requisição");
+          }
+        });
+    }).catch((error) => {
+      reject(error);
+    });
+  },
+};
+
+export { LoginService };
