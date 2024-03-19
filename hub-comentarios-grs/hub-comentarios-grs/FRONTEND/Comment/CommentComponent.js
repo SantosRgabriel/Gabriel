a86d382e3e2db96cf93fef78c3309bf6b3095a3d
SantosRgabriel <gabrielsantos.sgra@gmail.com>
2024-03-19 19:18:06 -0300

    update commentComponent

--- hub-comentarios-grs/hub-comentarios-grs/FRONTEND/Comment/CommentComponent.js
+++ hub-comentarios-grs/hub-comentarios-grs/FRONTEND/Comment/CommentComponent.js
@@ -1,6 +1,6 @@
-import { formatDate } from "../utils";
 import { CommentService } from "../services/comment.services.js";
-import { Comment } from "../models/comment.";
+import { Comment } from "../models/comment.model.js";
+
 const getInputComment = () => {
   return {
     author: document.getElementById("inputAuthor"),
@@ -32,11 +32,24 @@ const submitComment = (event) => {
 
 const loadComment = async () => {
   // Dados carregados da API
-  const data = await CommentService.apiGetComment();
-  const comments = data.map((comment) => {
-    comment = new Comment(comment);
-  });
-  displayComment(comments);
+  CommentService.apiGetComment()
+    .then((result) => {
+      const comments = result.map(
+        (comment) =>
+          new Comment(
+            comment.id,
+            comment.author,
+            comment.comment_text,
+            comment.created_at,
+            comment.updated_at
+          )
+      );
+      displayComment(comments);
+    })
+    .catch((error) => {
+      console.error(error);
+      alert(error);
+    });
 };
 
 const displayComment = (comments) => {
@@ -44,23 +57,19 @@ const displayComment = (comments) => {
   divFeed.innerHTML = ``;
   comments.forEach((item) => {
     const divDisplay = document.createElement("div");
+    let data = new Date(item.getCreatedAt());
     divDisplay.className = "d-flex text-body-secondary pt-3";
     divDisplay.innerHTML = `
-            <svg class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32"
-                xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32"
-                preserveAspectRatio="xMidYMid slice" focusable="false">
-                <title>comentário</title>
-                <rect width="100%" height="100%" fill="#007bff"></rect><text x="50%" y="50%" fill="#007bff"
-                    dy=".3em">32x32</text>
-            </svg>
-            <p class="pb-3 mb-0 small lh-sm border-bottom">
-                <strong class="d-block text-gray-dark">@${item.getAuthor()}
-                <span class="date-style badge text-bg-secondary">${formatDate(
-                  item.getCreated_at()
-                )}</span>
-                </strong>
-                ${item.getUpdated_at()}
-            </p>        
+    <div class="cardzin">
+    <div class="d-flex">
+        <p class="pb-0 mb-0 small lh-sm" style="word-break: break-all;">
+            <strong>@${item.getAuthor()}</strong>
+            <i>${item.getCommentText()}</i>     
+        </p>
+        
+    </div>
+    <small class="d-block text-end">${data.toLocaleDateString()} às ${data.getHours()}:${data.getMinutes()}</small>
+    </div>     
         `;
     divFeed.appendChild(divDisplay);
   });
